39ddc0b009baec9c0fa04f273c49414202a5a8a7
anna-left <anna_left@mail.ru>
2021-09-09 11:35:27 +0300

    feat: solve Transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,7 +13,55 @@ import { NotImplementedError } from '../extensions/index.js';
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-export default function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function transform(arr) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+
+    // --discard - next исключает следующий элемент массива из преобразованного массива.
+    //     --discard - prev исключает предыдущий элемент массива из преобразованного массива.
+    //     --double - next удваивает следующий элемент массива в преобразованном массиве.
+    //     --double - prev удваивает предыдущий элемент массива в преобразованном массиве.
+    if (!Array.isArray(arr)) {
+        throw new Error("\'arr\' parameter must be an instance of the Array!");
+    }
+    let newArr = [];
+    // let discard_next
+    let discard_next = 0;
+    for (let i = 0; i < arr.length; i++) {
+        let el = arr[i];
+        if (discard_next === 1) {
+            discard_next = 2;
+            continue;
+        }
+        switch (arr[i]) {
+            case '--discard-next':
+                discard_next = 1;
+                break;
+            case '--discard-prev':
+                if (discard_next !== 2 && i !== 0) {
+                    newArr.splice(newArr.length - 1);
+                }
+                break;
+            case '--double-next':
+                if (i + 1 < arr.length) {
+                    newArr.push(arr[i + 1]);
+                }
+
+                break;
+            case '--double-prev':
+                if (discard_next !== 2 && i !== 0) {
+                    newArr.push(newArr[newArr.length - 1]);
+                }
+                break;
+            default:
+                newArr.push(arr[i]);
+        }
+        discard_next = (discard_next === 1) ? 1 : 0;
+    }
+    return newArr;
 }
+
+let input = [1, 2, 3, '--double-next', 1337, '--double-prev', 4, 5];
+// output: [1, 2, 3, 4, 5]
+console.log((transform(input)));
