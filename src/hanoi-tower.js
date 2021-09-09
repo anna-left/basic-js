df2643bbe2a3bb0dce8d1d02d769c64931b492c3
anna-left <anna_left@mail.ru>
2021-09-09 10:30:02 +0300

    feat: solve hanoi-tower

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -14,7 +14,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * calculateHanoi(9, 4308) => { turns: 511, seconds: 427 }
  *
  */
-export default function calculateHanoi(/* disksNumber, turnsSpeed */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function calculateHanoi(disksNumber, turnsSpeed) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+
+    let obj = { turns: 0, seconds: 0 };
+    obj.turns = Math.pow(2, disksNumber) - 1;
+    obj.seconds = Math.floor(obj.turns * 3600 / turnsSpeed);
+    return obj;
 }
+
+// assert.deepEqual(calculateHanoi(48, 4040), { turns: 281474976710655, seconds: 250819286177811 });
+
+console.log(calculateHanoi(48, 4040));
