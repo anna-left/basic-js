a5718bd91f51d060666d70cac8d2f8a325a1c093
anna-left <anna_left@mail.ru>
2021-09-11 11:52:57 +0300

    feat: solve DNS stat

--- src/st-dns-stats.js
+++ src/st-dns-stats.js
@@ -22,7 +22,51 @@ import { NotImplementedError } from '../extensions/index.js';
  * }
  *
  */
-export default function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function getDNSStats(domains) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    let obj = {};
+    for (let i = 0; i < domains.length; i++) {
+        const el = domains[i];
+
+
+        let pos1 = el.lastIndexOf('.');
+        let str1 = el.slice(pos1, el.length);
+        if (obj[str1] === undefined) {
+            obj[str1] = 1;
+        } else { obj[str1] += 1 }
+        let pos2 = el.lastIndexOf('.', pos1 - 1);
+        if (pos2 < 0) {
+            let str2 = str1 + '.' + el.slice(0, pos1);
+            if (obj[str2] === undefined) {
+                obj[str2] = 1;
+            } else { obj[str2] += 1 }
+        } else {
+
+            let str2 = str1 + el.slice(pos2, pos1);
+            if (obj[str2] === undefined) {
+                obj[str2] = 1;
+            } else { obj[str2] += 1 }
+
+            let str3 = str2 + '.' + el.slice(0, pos2);
+            // if (obj[str3] === undefined) {
+            obj[str3] = 1;
+            // } else { obj[str2] += 1 }
+        }
+
+        // let pos3 = el.lastIndexOf('.', pos2 - 1);
+    }
+    return obj;
 }
+
+
+let a = '';
+// a = getDNSStats(['epam.com']);
+// , { '.com': 1, '.com.epam': 1 });
+// console.log(a);
+a = getDNSStats(['epam.com', 'info.epam.com']);
+//  { '.com': 2, '.com.epam': 2, '.com.epam.info': 1 });
+console.log(a);
+// a = getDNSStats([]);
+//  {});
+// console.log(a);
