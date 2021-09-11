b7d8f22f5d8bed0557770595aff47a9f714c028e
anna-left <anna_left@mail.ru>
2021-09-11 12:58:22 +0300

    feat: solve Encode line

--- src/st-encode-line.js
+++ src/st-encode-line.js
@@ -10,7 +10,55 @@ import { NotImplementedError } from '../extensions/index.js';
  * For aabbbc should return 2a3bc
  *
  */
-export default function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function encodeLine(str) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (!str) {
+        return '';
+    }
+    let arr = str.split('');
+    let result = '';
+    let curSymb = '';
+    let curSum = 0;
+    let el = '';
+    for (let i = 0; i <= arr.length; i++) {
+        if (i < arr.length) {
+            el = arr[i];
+
+        } else { el = '' }
+
+
+        if (curSymb === '') {
+            // curSymb = el;
+            // continue;
+        } else if (curSymb !== el) {
+            // curSum = +1;
+            if (curSum > 1) {
+                result = result + String(curSum);
+            }
+            result = result + curSymb;
+            curSum = 0;
+            // } else {
+
+        }
+        curSymb = el;
+        curSum += 1;
+    }
+
+    // let obj = arr.reduce(function(acc, el) {
+    //     acc[el] = (acc[el] || 0) + 1;
+    //     return acc;
+    // }, {});
+
+
+    // for (let key in obj) {
+    //     let n = (obj[key] === 1) ? '' : String(obj[key]);
+    //     result = result + key + n;
+    // }
+    return result;
+
 }
+let a = encodeLine('abbcca');
+// 'a2b2ca');
+// '4a4t'
+console.log(a);
