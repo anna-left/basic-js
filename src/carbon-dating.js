f198b8af331cad661041d2d6c8bf6b875c86fe97
anna-left <anna_left@mail.ru>
2021-09-12 11:34:46 +0300

    feat: solve Carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,22 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+export default function dateSample(sampleActivity) {
+    // throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
+    if (typeof(sampleActivity) !== "string") {
+        return false;
+    }
+    let sampleActivityNumber = parseFloat(sampleActivity);
+    if (sampleActivityNumber !== sampleActivityNumber) {
+        return false;
+    }
+    if (sampleActivityNumber <= 0 || sampleActivityNumber >= MODERN_ACTIVITY) {
+        return false;
+    }
+    let res = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivityNumber) * HALF_LIFE_PERIOD / Math.LN2);
+    return res;
 }
+
+let a = dateSample('one');
+console.log(a);
